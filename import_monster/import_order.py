2fd27a6198d69c90cde22e576dceb72c531a0f04
compu-github <yaoyuzuobao@163.com>
2021-06-01 00:21:31 -0700

    import_order revized

--- import_monster/import_order.py
+++ import_monster/import_order.py
@@ -2,26 +2,21 @@
 # dynamic_import_2.py
 import importlib
 from types import ModuleType
-from typing import List, Union
+from typing import Callable, List, Union
 
 
-def methods_importer(method_name: str, modules: List[Union[str, ModuleType]]) -> List[object]:
+def methods_importer(method_name: str, modules: List[Union[str, ModuleType]]) -> List[Callable]:
     for module in modules:
         try:
-
             if isinstance(module, ModuleType):
                 mod = module
             elif isinstance(module, str):
                 mod = importlib.import_module(module)
             else:
                 raise TypeError("Must be list of strings or ModuleType.")
-
             met = getattr(mod, method_name, None)
-
-            if met:
+            if met and isinstance(met, Callable):
                 return met
-
         except ImportError:
             continue
-
     return None
